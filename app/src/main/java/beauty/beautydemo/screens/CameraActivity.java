545b89711a29930af2d1a242fbdfc29eaf1edcdc
Lou Jiwei <jiwei.lou@foxmail.com>
2015-07-22 15:42:20 +0800

    取色按钮下调

--- app/src/main/java/beauty/beautydemo/screens/CameraActivity.java
+++ app/src/main/java/beauty/beautydemo/screens/CameraActivity.java
@@ -21,6 +21,7 @@ import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -61,6 +62,7 @@ public class CameraActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_camera);
         ButterKnife.inject(this);
 
